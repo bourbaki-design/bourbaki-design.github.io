69d31f444630a9c35bb3f5029cb8e9aac653e090
bourbaki-design <ito.gaku25010018@gmail.com>
2020-07-18 00:50:20 +0900

    aa

--- js/a.js
+++ js/a.js
@@ -4,7 +4,3 @@ var player = new Vimeo.Player('sample');
 player.on('play', function() {
     console.log('再生');
 });
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
